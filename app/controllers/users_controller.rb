7a48b1690e6368be7c977434860ad8c240cf035a
tbenbow-booth <tbenbow@chicagobooth.edu>
2024-07-28 01:54:03 +0000

    Partial show

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def index
+    matching_users = User.all
+    
+    @list_of_users = matching_users.order({ :username => :asc})
+
+    render({ :template => "user_templates/index"})
+  end
+
+  def show
+    # Parameters: {"path_username"=>"anisa"}
+
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({ :username => url_username})
+
+    @the_user = matching_usernames.first
+
+  render({ :template => "user_templates/show"})
+  end
+end
